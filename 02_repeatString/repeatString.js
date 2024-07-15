f839f221d0fc14f286c0e065cec10971377a987d
Oliver Sharman <4086182+olisharman@users.noreply.github.com>
2024-07-15 13:28:09 +0200

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfRepeat) {
+    let repeatedString = "";
+    if (numberOfRepeat < 0) {
+        return "ERROR";
+    }
+    else {
+    for (let i = 0; i < numberOfRepeat; i++) {
+    repeatedString += word;
+    }
+    return(repeatedString);
+}
 };
+
+console.log(repeatString);
 
 // Do not edit below this line
 module.exports = repeatString;
